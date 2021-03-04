51c5e878c5702119a6001ea3981852d69639ebd4
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2021-03-04 15:27:05 -0400

    Add test for getting default word pool

---
+++ tests/test_words.py
@@ -0,0 +1,16 @@
+"""Tests for `words` module."""
+
+# import pytest
+
+from stimpool.words import WordPoolCreator
+
+
+def test_get_default_pool() -> None:
+    """Test words._get_default_pool."""
+
+    shape_exp = (55457,)
+    word_pool_creator = WordPoolCreator()
+    word_pool = word_pool_creator._get_default_pool()
+    shape_obs = word_pool.shape
+
+    assert shape_obs == shape_exp
