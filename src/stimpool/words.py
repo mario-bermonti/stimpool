aae5866ff1b7cde5696601dfe07423a5cbc1f22c
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2021-03-04 15:25:59 -0400

    Use default word pool if no pool is provided

---
+++ src/stimpool/words.py
@@ -0,0 +1,36 @@
+"""Create word pools."""
+
+from pathlib import Path
+from typing import Iterable, Optional
+
+import pandas as pd
+
+ROOT_DIR = Path().resolve()
+
+
+class WordPoolCreator(object):
+    """Create word pools."""
+
+    def __init__(self, pool: Optional[Iterable] = None) -> None:
+        """Create a word pool.
+
+        Parameters
+        -----------
+        pool : Iterable
+            Word pool that will be used to create subpool (the default
+            is None, use default word pool)
+        """
+
+        if pool is None:
+            pool = self._get_default_pool()
+
+        self.pool_original: Iterable = pool
+
+    def _get_default_pool(self) -> Iterable:
+        """Get the default word pool."""
+
+        path = ROOT_DIR / "src" / "stimpool" / "words" / "es_PR.dic"
+        pool = pd.read_csv(path, squeeze=True)
+        pool.name = "original pool"
+
+        return pool
