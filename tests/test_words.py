70491b772c90e6dff6b0c67523d97595c2bab915
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2021-03-04 17:19:59 -0400

    Add test for normalizing words

--- tests/test_words.py
+++ tests/test_words.py
@@ -14,3 +14,25 @@ def test_get_default_pool():
     shape_obs = word_pool.shape
 
     assert shape_obs == shape_exp
+
+
+@pytest.mark.parametrize(
+    ("word_original", "word_expected"),
+    [
+        # none
+        ("perro", "perro"),
+        # upper
+        ("PErrO", "perro"),
+        # space
+        ("   perro   ", "perro"),
+        # mixed
+        ("  PErrO", "perro"),
+    ],
+)
+def test_normalize_word(word_original, word_expected):
+    """Test the _normalize_word with different cases."""
+
+    word_pool_creator = WordPoolCreator()
+    word_observed = word_pool_creator._normalize_word(word_original)
+
+    assert word_observed == word_expected
