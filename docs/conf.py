4d98159167fab87d0773dc638c399a9d58c67b75
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2021-03-09 22:38:31 -0400

    Remove options that don't apply to the current theme

--- docs/conf.py
+++ docs/conf.py
@@ -71,13 +71,8 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "github_user": "mario-bermonti",
-    "github_repo": "stimpool",
-    "github_banner": True,
-    "show_related": False,
-    "fixed_sidebar": True,
-}
+# html_theme_options = {
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
