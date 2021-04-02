32da24cf24f3a500a4eec25e456a9a0ae9da607d
Mario E. Bermonti Pérez <mario-bermonti@users.noreply.github.com>
2021-04-02 15:42:19 -0400

    List md files as accepted file extension

--- docs/conf.py
+++ docs/conf.py
@@ -53,8 +53,11 @@ autodoc_member_order = "bysource"
 #     'undoc-members': 'code,error_template',
 #     'exclude-members': '__dict__,__weakref__',
 # }
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
+
+source_suffix = ['.rst', '.md']  # file types to accept
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
